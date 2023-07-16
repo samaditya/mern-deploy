14180e81a1c397ae7a18435a583fd953ab61658f
samaditya jatar <samadityaj@gmail.com>
2023-07-16 23:08:49 +0530

    uri updated

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const { default: mongoose, Mongoose } = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/profilecards"
+// const mongoURI = "mongodb://localhost:27017/profilecards"
+const mongoURI = "mongodb+srv://samadityaj:wa6p4yHum7hOiML4@cluster0.0qxzlod.mongodb.net/?retryWrites=true&w=majority"
 
 const connect2Mongo = async ()=>{
     mongoose.connect(mongoURI)
