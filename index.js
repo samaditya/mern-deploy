5175d83181dbc4e1927066928b3b82d8fbc43882
samaditya jatar <samadityaj@gmail.com>
2023-07-18 14:41:22 +0530

    env updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 connect2Mongo();
 const app = express()
 app.use(cors())
-const port =  process.env.PORT ||5000
+const port =  process.env.BASE_URL 
 
 app.use(express.json())
 app.use(cors())
